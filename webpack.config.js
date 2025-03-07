1dbdf2a318272e53cbfc0db0775690fdcdf233e9
Erfan Abouie <erfangamer138787@gmail.com>
2025-03-07 12:16:04 +0330

    Fix: Webpack ( Babel Bug )

--- webpack.config.js
+++ webpack.config.js
@@ -8,25 +8,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-
     mode: 'production',
     devtool: 'source-map',
 
